df25f607667c999a4dc8fe3e8c85ff66491980e0
Eduardo Incrocci <edi4518@gmail.com>
2023-09-28 14:08:11 -0300

    Corregidos errors en la ejecucion del fetch async

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
-// const contenedorProductos = document.querySelector("#contenedor-productos");
-
+const contenedorProductos = document.querySelector("#contenedor-productos");
 let productos;
 
-fetch("js/productos.json")
-.then((response) => response.json())
-.then((json) => {
-  localStorage.setItem("listado", JSON.stringify(json));
-  });
-// Ahora lo obtengo con el getItem
+const listaProductos = async () => {
+      
+  productos = localStorage.getItem("productos");
+
+  if(productos == null){
+
+  const response = await fetch("json/productos.json");
+  
+  productos = await response.json();
+
+  localStorage.setItem("listado", JSON.stringify(productos));
+
+}
+ if(typeof productos == "string"){
+    productos = JSON.parse(productos);
+}
+    console.log(productos);
+    
+}
+
+listaProductos();
+
 const listadoProductos = JSON.parse(localStorage.getItem("listado"));
-// creo una cosntante para la ul en dnd voy a agregar los elementos
-const contenedorProductos = document.querySelector("#contenedor-productos");
 
 
-listadoProductos.forEach((producto) => {
+
+listadoProductos.forEach(producto => {
     let content = document.createElement("div");
-     // creo items respetando los nombres de bootstrap
      content.className = "col";
      content.innerHTML = `
      <div class="card h-100">
@@ -25,45 +38,36 @@ listadoProductos.forEach((producto) => {
                 alt="${producto.titulo}"
               />
               <div class="card-body">
-                <h4 class="card-title">${producto.titulo}</h4>
+                <h4 class="card-title">Lenovo IdeaPad 3i</h4>
                 <p class="card-text pt-3">${producto.descripcion}
                 </p>
               </div>
               <div class="card-footer">
-                <a href="detalles.html" target="_blank" 
-                  ><small class="text-body-secondary"
-                    ><i class="bi bi-zoom-in fw-bold fs-3"></i></small
-                ></a>
+              <a href="detalles.html" target="_blank" id="enlaces-detalles"
+                ><small class="text-body-secondary"
+                ><i class="bi bi-zoom-in fw-bold fs-3"></i></small
+              ></a>
               </div>
        `;
        
        contenedorProductos.append(content);
      });
-
-// const listaProductos = async () => {
-//   contenedorProductos.innerHTML = "";
-    
-//   productos = localStorage.getItem("productos");
-
-//   if(productos == null){
-
-//   const response = await fetch("js/productos.json");
   
-//   productos = await response.json();
 
-//   localStorage.setItem("productos", JSON.stringify(productos));
 
-// }
-//  if(typeof productos == "string"){
-//     productos = JSON.parse(productos);
-// }
-//     console.log(productos);
-    
-// }
 
-// listaProductos();
+// fetch("json/productos.json")
+// .then((response) => response.json())
+// .then((json) => {
+//   localStorage.setItem("listado", JSON.stringify(json));
+//   });
+// // Ahora lo obtengo con el getItem
+// const listadoProductos = JSON.parse(localStorage.getItem("listado"));
+// // creo una cosntante para la ul en dnd voy a agregar los elementos
+// const contenedorProductos = document.querySelector("#contenedor-productos");
+// const botonDetalles = document.querySelector("#enlaces-detalles");
 
-// listaProductos.forEach((producto) => {
+// listadoProductos.forEach((producto) => {
 //     let content = document.createElement("div");
 //      // creo items respetando los nombres de bootstrap
 //      content.className = "col";
@@ -75,12 +79,12 @@ listadoProductos.forEach((producto) => {
 //                 alt="${producto.titulo}"
 //               />
 //               <div class="card-body">
-//                 <h4 class="card-title">Lenovo IdeaPad 3i</h4>
+//                 <h4 class="card-title">${producto.titulo}</h4>
 //                 <p class="card-text pt-3">${producto.descripcion}
 //                 </p>
 //               </div>
 //               <div class="card-footer">
-//                 <a href="detalles.html" target="_blank" 
+//                 <a href="detalles.html" target="_blank" id="enlaces-detalles"
 //                   ><small class="text-body-secondary"
 //                     ><i class="bi bi-zoom-in fw-bold fs-3"></i></small
 //                 ></a>
@@ -89,4 +93,9 @@ listadoProductos.forEach((producto) => {
        
 //        contenedorProductos.append(content);
 //      });
-  
+
+     
+
+
+     
+
